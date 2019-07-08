5c3b5060b96a70aa51dc0fe3bec2fea13b8082a8
rolandrajko <drajko.roland@gmail.com>
2019-07-08 20:17:54 +0200

    Added docker-secrets.sh

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -2,19 +2,21 @@
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+    set -- php-fpm "$@"
 fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
     PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-	fi
+    if [ "$APP_ENV" != 'prod' ]; then
+        PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+    fi
     ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
     #mkdir -p var/cache var/log
-	#setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	#setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    #setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    #setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+    source docker-secrets.sh
 
     if [ "$APP_ENV" != 'prod' ]; then
         composer install --ansi --prefer-dist --no-progress --no-suggest --no-interaction
---
+++ php/docker-secrets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+while read -r envVar; do
+    file_env "${envVar%_FILE}"
+done < <(printenv | cut -d'=' -f1 | grep '_FILE$')
