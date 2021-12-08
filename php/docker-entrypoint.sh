a5269963587e0aaf93cb4deed1577016969bb654
rolandrajko <drajko.roland@gmail.com>
2021-12-08 19:07:11 +0100

    Dynamically set up PM

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -e
 
+set_up_pm() {
+    local cpu_cores=${FPM_CPU_CORES:-1}
+    local total_ram=${FPM_TOTAL_RAM:-1024}
+    local process_size=${FPM_PROCESS_SIZE:-32}
+
+    if [[ $(echo "${cpu_cores}<0.5" | bc) -eq 1 ]]; then
+        echo 'Misconfiguration: $FPM_CPU_CORES should be greater than or equal to 0.5'
+        exit 1
+    fi
+
+    export FPM_MAX_CHILDREN=$(($total_ram / $process_size))
+    export FPM_MIN_SERVERS=$(echo "${cpu_cores}*2/1" | bc)
+    export FPM_MAX_SERVERS=$(echo "${cpu_cores}*4/1" | bc)
+
+    if [[ $FPM_MAX_SERVERS -gt $FPM_MAX_CHILDREN ]]; then
+        echo 'Misconfiguration: $FPM_TOTAL_RAM / $FPM_PROCESS_SIZE should be greater than or equal to $FPM_CPU_CORES * 4'
+        exit 1
+    fi
+
+    local www_conf=/usr/local/etc/php-fpm.d/zz-www.conf
+    envsubst < $www_conf > /tmp/zz-www.conf && mv /tmp/zz-www.conf $www_conf
+}
+
+set_up_pm
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
     set -- php-fpm "$@"
